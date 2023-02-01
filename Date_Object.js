7e12b3abcda7dbeaf1abf01d66721e79ead46595
Amanullah Akon <amanullah15-2447@diu.edu.bd>
2023-02-01 23:10:31 +0600

    Date object

---
+++ Date_Object.js
@@ -0,0 +1,17 @@
+var date = new Date();
+document.write("Using Date Objects in JavaScript : "+"<br>"+"<br>");
+
+var year = date.getFullYear();
+document.write("This year is : "+year + "<br>");
+
+var day = date.getDay();
+document.write("This day is : "+day + "<br>");
+
+var totaldate = date.getDate();
+document.write("This Date is : "+totaldate + "<br>");
+
+var Hours = date.getHours();
+document.write("This Hours is : "+Hours + "<br>");
+
+var Month = date.getMonth();
+document.write("This Month is : "+Month + "<br>");
