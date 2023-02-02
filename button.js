bd10a7411b881b5fe7f739a62a56075a5d206ad3
Amanullah Akon <amanullah15-2447@diu.edu.bd>
2023-02-03 00:44:12 +0600

    Audio Add in button

---
+++ button.js
@@ -0,0 +1,25 @@
+for (var i = 0; i < 3; i++) {
+  document
+    .querySelectorAll(".button1")
+    [i].addEventListener("click", function () {
+      var text = this.innerHTML;
+      console.log(text);
+
+      switch (text) {
+        case "Audio1":
+          var audio = new Audio("Sounds/Buker jomano batha.mp3");
+          audio.play();
+          break;
+
+        case "Audio2":
+          var audio = new Audio("Sounds/Amon chena cinbe loke.mp3");
+          audio.play();
+          break;
+
+        case "Audio3":
+          var audio = new Audio("Sounds/Tumay dekle mone hoy.mp3");
+          audio.play();
+          break;
+      }
+    });
+}
--- querySelector.js
+++ querySelector.js
@@ -1,6 +1,6 @@
 //quertSelector
 
-document.querySelector("b").innerHTML = ("I am not a good lerner.");//quertSelector using tag.
-document.querySelector("i").innerHTML = ("That's not a fun.😛");//quertSelector using tag.
-document.querySelector(".aman").innerHTML = ("This is not a headding.");//quertSelector using class.
-document.querySelector("#aman2").innerHTML = ("This is a testing parpes.");//quertSelector using id.
+document.querySelector("b").innerHTML = "I am not a good lerner."; //quertSelector using tag.
+document.querySelector("i").innerHTML = "That's not a fun.😛"; //quertSelector using tag.
+document.querySelector(".aman").innerHTML = "This is not a headding."; //quertSelector using class.
+document.querySelector("#aman2").innerHTML = "This is a testing parpes."; //quertSelector using id.
