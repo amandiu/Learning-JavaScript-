d6b2262cf2ffa3b2d14bf87e09b209d13dc28e95
Amanullah Akon <amanullah15-2447@diu.edu.bd>
2023-02-04 21:04:40 +0600

    Downwards-querySellector

--- DOM_Elements.js
+++ DOM_Elements.js
@@ -9,3 +9,11 @@ document.getElementById("aman1").innerHTML = "I am not a good student"; //getEle
 document.getElementsByTagName("p")[1].innerHTML = "Hi Sugandha.😊"; //getElementsByTagName
 document.getElementsByClassName("had4")[0].innerHTML = "That's a fun.😛"; //getElementsByClassName
 document.getElementsByClassName("smile")[0].innerHTML = "Obviously.!! This is my mind.😊"; //getElementsByClassName
+
+//Downwards-querySellector / querySellectorAll
+
+document.querySelector(".students_list").querySelector(".student_a").innerHTML=("Md.Amanullah");
+
+document.querySelector(".students_list").querySelector(".student_b").innerHTML=("Md.Sagor Alom");
+
+document.querySelector(".student_c").innerHTML=("Sagor Alom");
